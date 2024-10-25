bf415460ab526605f5b51a829ea0333ea0630dc9
arun-kbharathan-e3768 <arun.bharathan@freshworks.com>
2024-10-25 17:31:06 +0530

    Check if file exits and is not empty, it it exists it prints some messages and add to kafka opts

--- docker-images/kafka-based/kafka/scripts/kafka_run.sh
+++ docker-images/kafka-based/kafka/scripts/kafka_run.sh
@@ -116,7 +116,8 @@ export KAFKA_OPTS
 # Configure Garbage Collection logging
 . ./set_kafka_gc_options.sh
 
-if [ -f "/tmp/kafka_server_sasl_plain_jaas.conf" ]; then
+if [ -s "/tmp/kafka_server_sasl_plain_jaas.conf" ]; then
+  echo "Using SASL/PLAIN authentication with JAAS configuration"
   KAFKA_OPTS="${KAFKA_OPTS} -Djava.security.auth.login.config=/tmp/kafka_server_sasl_plain_jaas.conf"
 fi
 
